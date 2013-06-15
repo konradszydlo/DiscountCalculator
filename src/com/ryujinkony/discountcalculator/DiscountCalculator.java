67cf4c724cfd80d0e43267955eb4b361d47190dd
Konrad Szydlo <konrad_szydlo@yahoo.co.uk>
2013-06-15 12:50:48 +0100

    mod to class.

--- src/com/ryujinkony/discountcalculator/DiscountCalculator.java
+++ src/com/ryujinkony/discountcalculator/DiscountCalculator.java
@@ -14,7 +14,7 @@ public class DiscountCalculator extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		// Inflate the menu; this adds items to the action bar if it is present. 
 		getMenuInflater().inflate(R.menu.discount_calculator, menu);
 		return true;
 	}
